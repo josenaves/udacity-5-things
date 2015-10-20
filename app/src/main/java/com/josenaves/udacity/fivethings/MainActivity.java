07b4d4df685cbecfb4d3ca1bfed8c167d15e961b
José Naves <josenaves@gmail.com>
2015-10-20 16:37:59 -0200

    First steps

--- app/src/main/java/com/josenaves/udacity/fivethings/MainActivity.java
+++ app/src/main/java/com/josenaves/udacity/fivethings/MainActivity.java
@@ -2,6 +2,8 @@ package com.josenaves.udacity.fivethings;
 
 import java.util.Locale;
 
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -18,15 +20,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each
+     * of the sections. We use a {@link FragmentPagerAdapter} derivative, which will keep
+     * every loaded fragment in memory. If this becomes too memory intensive, it may be best to
+     * switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
 
@@ -48,34 +48,9 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    /**
+     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -94,8 +69,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 5 total pages.
+            return 5;
         }
 
         @Override
@@ -108,6 +83,10 @@ public class MainActivity extends AppCompatActivity {
                     return getString(R.string.title_section2).toUpperCase(l);
                 case 2:
                     return getString(R.string.title_section3).toUpperCase(l);
+                case 3:
+                    return getString(R.string.title_section4).toUpperCase(l);
+                case 4:
+                    return getString(R.string.title_section5).toUpperCase(l);
             }
             return null;
         }
@@ -118,14 +97,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public static class PlaceholderFragment extends Fragment {
         /**
-         * The fragment argument representing the section number for this
-         * fragment.
+         * The fragment argument representing the section number for this fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
         /**
-         * Returns a new instance of this fragment for the given section
-         * number.
+         * Returns a new instance of this fragment for the given section number.
          */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
@@ -141,9 +118,36 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            @LayoutRes
+            int layout = R.layout.fragment_word_1;
+
+            Bundle args = getArguments();
+            switch (args.getInt(ARG_SECTION_NUMBER)) {
+                case 1:
+                    layout = R.layout.fragment_word_1;
+                    break;
+
+                case 2:
+                    layout = R.layout.fragment_word_2;
+                    break;
+
+                case 3:
+                    layout = R.layout.fragment_word_2;
+                    break;
+
+                case 4:
+                    layout = R.layout.fragment_word_2;
+                    break;
+
+                case 5:
+                    layout = R.layout.fragment_word_2;
+            }
+            View rootView = inflater.inflate(layout, container, false);
+            TextView textView = (TextView)rootView.findViewById(R.id.section_label);
+            textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
+
             return rootView;
         }
     }
-
 }
